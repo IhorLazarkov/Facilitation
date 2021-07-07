edfb3099bc5e440ed45ff36c53c4a72d66168ac9
Ihor_Lazarkov <ilazarkov@gmail.com>
2021-07-06 19:34:15 -0500

    finish adding of graphical part. in future it should be more flexible to represent and manipulate with data.

--- lego-scanner/src/main/java/org/legoscanner/web/controllers/GenericController.java
+++ lego-scanner/src/main/java/org/legoscanner/web/controllers/GenericController.java
@@ -1,19 +1,31 @@
 package org.legoscanner.web.controllers;
 
+import info.facilitator.bean.LegoBean;
+import info.facilitator.persister.SessionProvider;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class GenericController {
 
     @GetMapping("/")
     public ModelAndView index() {
+        final List<LegoBean> legos = new LinkedList<>();
+        SessionProvider.withSession(session -> {
+
+            Query<LegoBean> query = session.createQuery("From LegoBean order by legoName, date desc", LegoBean.class);
+            legos.addAll(query.getResultList());
+
+        });
         ModelAndView mv = new ModelAndView("home");
-        mv.addObject("items", Arrays.asList("item1", "item2"));
-        System.out.println("index");
+        mv.addObject("legos", legos);
+        System.out.println(legos);
         return mv;
     }
 }
