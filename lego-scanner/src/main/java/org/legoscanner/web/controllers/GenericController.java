535a135f38791b83dfd770c95132472e89d14991
IhorLazarkov <86078653+IhorLazarkov@users.noreply.github.com>
2021-08-02 09:45:24 -0500

    Decouple data fetcher (#7)
    * added DefaultFetcher this implements fetching of all data from Lego table
    
    * minor cleanup

--- lego-scanner/src/main/java/org/legoscanner/web/controllers/GenericController.java
+++ lego-scanner/src/main/java/org/legoscanner/web/controllers/GenericController.java
@@ -3,26 +3,28 @@ package org.legoscanner.web.controllers;
 import info.facilitator.bean.LegoBean;
 import info.facilitator.persister.SessionProvider;
 import io.swagger.annotations.ApiOperation;
-import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Function;
 
 @RestController
 public class GenericController {
+
+    @Autowired
+    private Function<Session, List<LegoBean>> fetcher;
 
     @ApiOperation(value = "Get all data", response = ModelAndView.class)
     @GetMapping("/")
     public ModelAndView index() {
         final List<LegoBean> legos = new LinkedList<>();
         SessionProvider.withSession(session -> {
-
-            Query<LegoBean> query = session.createQuery("From LegoBean order by legoName, date desc", LegoBean.class);
-            legos.addAll(query.getResultList());
-
+            legos.addAll(fetcher.apply(session));
         });
         ModelAndView mv = new ModelAndView("home");
         mv.addObject("legos", legos);
